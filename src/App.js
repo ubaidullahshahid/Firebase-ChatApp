f1c496f285cfae89c229afe86ea5990432736c5c
ubaidullahshahid <146563446+ubaidullahshahid@users.noreply.github.com>
2024-11-10 12:37:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ const App = () => {
         dispatch(setLoading());
       } else {
         dispatch(setCurrentUser(null));
+        dispatch(setLoading());
       }
     });
     return () => unsubscribe();
